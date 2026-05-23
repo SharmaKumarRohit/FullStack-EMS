41a4eb1e315ee4c9af7ca47e838cb0c4203f7dac
SharmaKumarRohit <sharmaeditx375@gmail.com>
2026-05-23 20:47:16 +0530

    backend completed

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -1,5 +1,6 @@
 import Employee from "../models/Employee.js";
 import Attendance from "../models/Attendance.js";
+import { inngest } from "../inngest/index.js";
 
 // Clock in/out for employee
 // POST /api/attendance
@@ -30,6 +31,14 @@ export const clockInOut = async (req, res) => {
         date: today,
         checkIn: now,
         status: isLate ? "LATE" : "PRESENT",
+      });
+
+      await inngest.send({
+        name: "employee/check-out",
+        data: {
+          employeeId: employee._id,
+          attendanceId: attendance._id,
+        },
       });
       return res.json({ success: true, type: "CHECK_IN", data: attendance });
     } else if (!existing.checkOut) {
